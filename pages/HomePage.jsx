4ef7006e8306194045c34215d4dd92234629f759
keyboard <jom184750@gmail.com>
2025-09-04 17:23:16 +0300

    books clone frontend

---
+++ pages/HomePage.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='home-page'>
+      Book store HOME PAGE
+    </div>
+  )
+}
+export default HomePage
+
+
