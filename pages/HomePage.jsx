fbb6004ad68c609fdf8108d609e5c55fcbb0e529
keyboard <jom184750@gmail.com>
2025-09-05 08:22:07 +0300

    h2 testing

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -9,6 +9,7 @@ const HomePage = () => {
     <div className='home-page'>
       Book-Store HOME PAGE 2 test
       <h1>H1 test for vercel</h1>
+      <h2>h2 testing for vercel</h2>
     </div>
   )
 }
