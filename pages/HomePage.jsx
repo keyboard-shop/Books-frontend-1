f0f598628339abc12f83d3f0429e2f417ad28da6
keyboard <jom184750@gmail.com>
2025-09-05 13:09:48 +0300

    error

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -7,10 +7,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='home-page'>
-      Book-Store HOME PAGE 2 test
-      <h1>H1 test for vercel</h1>
-      <h2>h2 testing for vercel</h2>
-      <h3>got the changes from vs code to Github to Vercel</h3>
+      Book-Store HOME PAGE
     </div>
   )
 }
