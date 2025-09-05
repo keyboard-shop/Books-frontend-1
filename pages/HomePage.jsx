d9ef494f689c67fc1fa93baf4f3bbec772ef827a
keyboard <jom184750@gmail.com>
2025-09-05 13:59:24 +0300

    message to the teacher

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -7,8 +7,17 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='home-page'>
-      <h2>HELLO!</h2>
-      Book Store HOME page
+      <h2>Hello, Teacher!</h2>
+      <h4>
+      If you are reading this message, it means that I have managed to deploy this website on the Internet.
+       I deployed both the frontend and backend folders on Vercel.
+        I was unable to deploy the backend on Render, as it always asked for my payment card, even though I selected the free plan:
+        select free plan - deploy - payment card.
+      </h4>
+      <br />
+      <h4>
+      Let's discuss the results and the code in our lesson.
+      </h4>
     </div>
   )
 }
