1e3a93307281ecf6e86e44e19467b3a52018c406
keyboard <jom184750@gmail.com>
2025-09-07 08:48:57 +0300

    simple 777 comment

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='home-page'>
-      <h2>Hello, Teacher!</h2>
+      <h2>Hello 777!</h2>
       <h4>
       If you are reading this message, it means that I have managed to deploy this website on the Internet.
        I deployed both the frontend and backend folders on Vercel.
